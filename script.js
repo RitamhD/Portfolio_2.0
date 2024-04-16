bb891ca384610a3b858d8e56409fdb5b54ae68f2
RitamhD <hatiritam03@gmail.com>
2024-04-16 12:56:47 +0530

    Commited

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+var navbarButtons = document.querySelectorAll('.nav_bar a');
 
-for(var i=0; i<document.querySelectorAll('a').length; i++){
-    document.querySelectorAll('a')[i].addEventListener("mouseover",function(){
+for(var i=0; i<navbarButtons.length; i++){
+        navbarButtons[i].addEventListener("mouseover", function(){   
         this.classList.add("a_hover");
+        var audio = new Audio("hover_audio2.mp3");
+        audio.play();
     });
-    document.querySelectorAll('a')[i].addEventListener("mouseout",function(){
+        navbarButtons[i].addEventListener("mouseout", function(){
         this.classList.remove("a_hover");
-    })
+    });
 }
-document.querySelector('#contact').addEventListener("click",function(){
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener("click", function() {
+      var audio = new Audio("mouse_click2.mp3")
+      audio.play();
+    });
+  });
+  
+document.querySelector('#contact').addEventListener("click", function(){
     document.querySelector('.contact_logos').classList.add('contact_logos_clicked');
-})
+});

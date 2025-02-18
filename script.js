fcc74fc81ab30902983ca3cbf889e8af20a96e3c
RitamhD <hatiritam03@gmail.com>
2025-02-18 18:47:55 +0530

    Merge branch 'main' of https://github.com/RitamhD/Portfolio_2.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//Navbar:-
 let navbarButtons = document.querySelectorAll('.nav_bar a');
-
 for(var i=0; i<navbarButtons.length; i++){
         navbarButtons[i].addEventListener("mouseenter", function(){   
         this.classList.add("a_hover");
@@ -16,7 +16,7 @@ document.querySelectorAll('a').forEach(link => {
       audio.play();
     });
   });
-  
+//Contact Logos:- 
 document.querySelector('#contact').addEventListener("click", function(){
     document.querySelector('.contact_logos').classList.add('contact_logos_clicked');
 });
@@ -32,8 +32,8 @@ function hideStartContent(){
   document.getElementById('content').style.display = 'block';
     document.body.style.background = 'rgb(20, 19, 19)';
 };
-
-let images = document.querySelectorAll('.parent img');
+//Carousel Images:-
+let images = document.querySelectorAll('#carousel img');
 let buttons = document.getElementsByClassName('button');
 let isProcessing = false;
 
@@ -47,7 +47,6 @@ document.querySelector('.right').addEventListener("click", () => {
         isProcessing = false;
     }, 500); // delay
 });
-
 document.querySelector('.left').addEventListener("click", () => {
     if (isProcessing) {
         return;
@@ -58,7 +57,6 @@ document.querySelector('.left').addEventListener("click", () => {
         isProcessing = false;
     }, 500); // delay
 });
-
 function swapStyles(sourceIndex, targetIndex1, targetIndex2){
     let tempStyles = window.getComputedStyle(images[sourceIndex]);
     let copiedStyles1 = window.getComputedStyle(images[targetIndex1]);
@@ -68,9 +66,16 @@ function swapStyles(sourceIndex, targetIndex1, targetIndex2){
     copyStyles(copiedStyles2, images[targetIndex1]);
     copyStyles(tempStyles, images[targetIndex2]);
 }
-
 function copyStyles(copied_styles, target_element){
     for(let pr of copied_styles){
         target_element.style[pr] = copied_styles[pr];
     }
 }
+
+// let carouselImages = document.querySelectorAll('.carousel_img');
+// for(var i=0; i<carouselImages.length; i++){
+//     carouselImages[i].addEventListener("mouseover", function(){
+//         console.log(this.lastElementChild);
+//         this.lastElementChild.classList.add('onHover');
+//     })
+// }
